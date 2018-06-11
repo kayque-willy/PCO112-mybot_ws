45b1a3bc00355ede7a2feebc5f795f163651c21a
Kayque Oliveira <kayque-willy@hotmail.com>
2018-06-11 14:47:10 -0300

    Adição da detecção de circulos pela camera

--- src/mybot_control/src/camerasub.cpp
+++ src/mybot_control/src/camerasub.cpp
@@ -1,12 +1,38 @@
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <cv_bridge/cv_bridge.h>
+#include <iostream>
+#include <stdio.h>
 
 void imageCallback(const sensor_msgs::ImageConstPtr& msg){
 	try{
-		cv::Mat frame = cv_bridge::toCvShare(msg, "bgr8")->image;	
-		cv::imshow("view", frame);
+		cv::Mat gray, src = cv_bridge::toCvShare(msg, "bgr8")->image;
+		std::ostringstream oss;	
+		cv::vector<cv::Vec3f> circles;
+		
+		//Converte para escala cinza
+		cv::cvtColor( src, gray, CV_BGR2GRAY );
+		 
+		// Reduz o ruido na detecao do circulo
+		cv::GaussianBlur( gray, gray, cv::Size(9, 9), 2, 2 );
+		 
+		// Aplica a Hough Transform para encontrar circulos
+		cv::HoughCircles( gray, circles, CV_HOUGH_GRADIENT, 1, 30, 200, 50, 0, 0 );
+		
+		// Desenha os circulos detectados
+		for( size_t i = 0; i < circles.size(); i++ ){
+			  cv::Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+			  int radius = cvRound(circles[i][2]);     
+			  cv::circle( src, center, 3, cv::Scalar(0,255,0), -1, 8, 0 );// circle center     
+			  cv::circle( src, center, radius, cv::Scalar(0,0,255), 3, 8, 0 );// circle outline
+			  oss << "center : " << center << "\nradius : " << radius << std::endl;
+			  ROS_INFO_STREAM( oss.str() );
+		}
+	 
+		// Mostra os resultados
+		cv::imshow( "View", src );
 		cv::waitKey(30);
 	}catch (cv_bridge::Exception& e){
 		ROS_ERROR("Could not convert from '%s' to 'bgr8'.",
@@ -17,11 +43,11 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg){
 int main(int argc, char **argv){
 	ros::init(argc, argv, "camerasub");
 	ros::NodeHandle nh;
-	cv::namedWindow("view");
+	cv::namedWindow( "View");
 	cv::startWindowThread();
 	image_transport::ImageTransport it(nh);
 	image_transport::Subscriber sub = it.subscribe("/mybot/camera1/image_raw", 1, &imageCallback);
 	ros::spin();
-	cv::destroyWindow("view");
+	cv::destroyWindow("View");
 	return 0;
 }
